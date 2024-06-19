9a0c73209f3330fcf015a52d5a46da7caec32e71
domasco01 <124378575+domasco01@users.noreply.github.com>
2024-06-19 09:44:18 +0200

    Delete loaderFunction.js

--- loaderFunction.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router-dom"
-export async function loader(){
-    const isLogged=false;
-
-    if (!isLogged){
-        throw redirect("/login?message=You must log in first")
-    }
-    return null
-}
